fb0df1d0952f5be18697f84731d8e32cccc69440
hendra.saputra <hendra.saputra@workday.com>
2020-04-14 19:42:43 +0100

    Use JSON as logging format

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,26 +2,30 @@ import com.lightbend.cinnamon.sbt.Cinnamon
 import sbt._
 
 object Version {
-  val akkaVer         = "2.6.3"
-  val logbackVer      = "1.2.3"
-  val scalaVer        = "2.13.1"
-  val scalaParsersVer = "1.1.2"
-  val scalaTestVer    = "3.1.0"
+  val akkaVer           = "2.6.3"
+  val logbackVer        = "1.2.3"
+  val logbackContribVer = "0.1.5"
+  val scalaVer          = "2.13.1"
+  val scalaParsersVer   = "1.1.2"
+  val scalaTestVer      = "3.1.0"
 
 }
 
 object Dependencies {
   val dependencies = Seq(
-    "com.typesafe.akka"       %% "akka-actor"                 % Version.akkaVer,
-    "com.typesafe.akka"       %% "akka-slf4j"                 % Version.akkaVer,
-    "ch.qos.logback"           % "logback-classic"            % Version.logbackVer,
-    "org.scala-lang.modules"  %% "scala-parser-combinators"   % Version.scalaParsersVer,
-    "com.lightbend.akka" %% "akka-diagnostics" % "1.1.12",
+    "com.typesafe.akka"         %% "akka-actor"                 % Version.akkaVer,
+    "com.typesafe.akka"         %% "akka-slf4j"                 % Version.akkaVer,
+    "ch.qos.logback"             % "logback-classic"            % Version.logbackVer,
+    "ch.qos.logback.contrib"     % "logback-jackson"            % Version.logbackContribVer,
+    "ch.qos.logback.contrib"     % "logback-json-classic"       % Version.logbackContribVer,
+    "org.scala-lang.modules"    %% "scala-parser-combinators"   % Version.scalaParsersVer,
+    "com.lightbend.akka"        %% "akka-diagnostics"           % "1.1.12",
     Cinnamon.library.cinnamonAkka,
     Cinnamon.library.cinnamonJvmMetricsProducer,
     Cinnamon.library.cinnamonPrometheus,
     Cinnamon.library.cinnamonPrometheusHttpServer,
-    "com.typesafe.akka"       %% "akka-testkit"               % Version.akkaVer % "test",
-    "org.scalatest"           %% "scalatest"                  % Version.scalaTestVer % "test"
+    "com.typesafe.akka"         %% "akka-testkit"               % Version.akkaVer % "test",
+    "org.scalatest"             %% "scalatest"                  % Version.scalaTestVer % "test",
+    "com.fasterxml.jackson.core" % "jackson-databind"           % "2.10.3"
   )
 }
